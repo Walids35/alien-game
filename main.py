96fc0bb5a4fa120d610236ed6add4a0acbeb666e
walid <wsiala4@gmail.com>
2022-12-28 19:41:24 +0100

    Game final Version

---
+++ main.py
@@ -0,0 +1,43 @@
+import pygame
+from pygame.sprite import Group
+from settings import Settings
+from ship import Ship
+import game_functions as gf
+from game_stats import GameStats
+from button import Button
+from scoreboard import Scoreboard
+
+
+def run_game():
+    # Initialize game and create a screen object
+    pygame.init()
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_height))
+    pygame.display.set_caption("Alien Invasion")
+
+    play_button = Button(ai_settings, screen, "Play")
+
+    # Make a ship
+    stats = GameStats(ai_settings)
+    sb = Scoreboard(ai_settings, screen, stats)
+    ship = Ship(ai_settings, screen)
+    bullets = Group()
+    aliens = Group()
+
+    # Create the fleet of aliens.
+    gf.create_fleet(ai_settings, screen, ship, aliens)
+
+    # Start the main loop for the game
+    while True:
+        # Function to retrieve user events
+        gf.check_events(ai_settings, screen, stats, play_button, aliens, ship, bullets)
+
+        if stats.game_active:
+            ship.update()
+            bullets.update()
+            gf.update_bullets(ai_settings, screen, stats, sb, ship, aliens, bullets)
+            gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
+
+        gf.update_screen(ai_settings, screen, stats, sb, ship, aliens, bullets, play_button)
+
+run_game()
